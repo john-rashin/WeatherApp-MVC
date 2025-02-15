73d2bfb52bb5c354ac43501365327b70624ad18c
john-rashin <lomoyajohnrashin@gmail.com>
2025-02-15 14:54:39 +0800

    Initialized ASP.NET Core MVC Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WeatherApp_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
